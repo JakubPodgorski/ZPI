3a591665515868407429abbd005ce5a943c4c858
Chase <Chase@ChasePC>
2015-01-25 21:05:41 +0100

    Constants

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -3,7 +3,7 @@ package Utils;
 public class Constants {
 
 	
-	public static String SELECT_EARNINGS = "SELECT CENAZMARZA - CENA From OPCJAOFERTY WHERE TERMINOD >= ? AND TERMINDO <= ?";
+	public static String SELECT_EARNINGS = "SELECT CENAZMARZA - CENA, ILOSCOSOBWPOKOJU From OPCJAOFERTY WHERE TERMINOD >= ? AND TERMINDO <= ?";
   	public static String SELECT_OFFERS_TIMESTAMP =  "SELECT * FROM OPCJAOFERTY s1, OPISOFERTY s2 WHERE s1.FK_OPISOFERTY = s2.ID AND s1.TERMINOD >= ? AND s1.TERMINDO <= ?";
     public static String SELECT_RESERVATIONS = "select R.ID, R.IMIE, R.NAZWISKO, R.KRAJ, R.MIASTO, R.ULICA, R.NUMERDOMU, R.KODPOCZTOWY, R.MAIL, R.TELEFON, R.STATUS, OPCJA.TERMINOD, OPCJA.TERMINDO, OPCJA.CENA, OPCJA.CENAZMARZA, OPCJA.ILOSCOSOBWPOKOJU, OPIS.NUMEROFERTY, OPIS.MIASTO AS OFERTA_MIASTO, OPIS.KRAJ AS OFERTA_KRAJ FROM REZERWACJA R INNER JOIN OPCJAOFERTY OPCJA ON R.FK_OPCJAOFERTY = OPCJA.ID INNER JOIN OPISOFERTY OPIS ON OPCJA.FK_OPISOFERTY = OPIS.ID";
     public static String SELECT_ALL_OFFERS = "SELECT s1.TYTUL, s1.OPIS, s1.KRAJ, s1.MIASTO, s1.OCENA, s1.NUMEROFERTY, s2.* FROM OPISOFERTY s1, OPCJAOFERTY s2 WHERE s1.ID = s2.FK_OPISOFERTY";
@@ -20,10 +20,10 @@ public class Constants {
   	public static String DB_PHOTO = "ZDJECIE";
   	public static String DB_MARK = "OCENA";
   	public static String DB_ID = "ID";
-
+  	public static String DB_PERSON_COUNT = "ILOSCOSOBWPOKOJU";
 
 	
-	public final static String columnNamesBilans[] = { "Numer oferty", "Termin", "Miasto", "Koszt w³asny", "Cena", "Zarezerwowano" };
+	public final static String columnNamesBilans[] = { "Numer oferty", "Termin", "Miasto", "Koszt w³asny", "Cena", "Zarezerwowano", "Zysk" };
 	public final static String columnNamesOffers[] = { "Numer oferty", "Tytu³", "Opis", "Kraj", "Miasto", "Wolne miejsca", "Cena za miejsce", "Ocena" };
 	
 	
